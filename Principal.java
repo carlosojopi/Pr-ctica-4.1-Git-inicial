21548dc1a0285f810a368e2ac03c0d0b17c994ef
Carlosojopi <7876805@alu.murciaeduca.es>
2024-02-06 11:39:43 +0100

    Añadido la modificacion de la clase Principal

--- Principal.java
+++ Principal.java
@@ -1,8 +1,11 @@
 //paso 6
 
 //paso 10
+
+//paso 14
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 public class Principal {
 
@@ -12,6 +15,12 @@ public class Principal {
 
         // Llama al método para mostrar la fecha y hora actual
         mostrarFechaYHoraActual();
+
+        // Llama al método para crear el array de Usuarios
+        Usuario[] usuarios = crearArrayUsuarios(3);
+
+        // Muestra el contenido de cada Usuario en el array
+        mostrarUsuarios(usuarios);
     }
 
     // Método para mostrar la fecha y hora actual
@@ -27,5 +36,36 @@ public class Principal {
 
         // Muestra la fecha y hora actual
         System.out.println("Fecha y hora actual: " + fechaYHoraFormateada);
+    }
+
+    public static Usuario[] crearArrayUsuarios(int numeroUsuarios) {
+        
+		Usuario[] usuarios = new Usuario[numeroUsuarios];
+
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < numeroUsuarios; i++) {
+            usuarios[i] = new Usuario();
+            System.out.println("Ingrese información para el Usuario " + (i + 1) + ":");
+            System.out.print("Ingrese el Nombre: ");
+            usuarios[i].nombre = scanner.nextLine();
+            System.out.print("Ingrese los Apellidos: ");
+            usuarios[i].apellidos = scanner.nextLine();
+            System.out.print("Ingrese su Email: ");
+            usuarios[i].email = scanner.nextLine();
+        }
+
+        return usuarios;
+    }
+
+    // Método para mostrar el contenido de cada Usuario en el array
+    public static void mostrarUsuarios(Usuario[] usuarios) {
+        System.out.println("\nLa informacion de los usuarios es la siguiente:");
+
+        for (Usuario usuario : usuarios) {
+            System.out.println("Nombre: " + usuario.nombre);
+            System.out.println("Apellidos: " + usuario.apellidos);
+            System.out.println("Email: " + usuario.email + "\n");
+        }
     }
 }
